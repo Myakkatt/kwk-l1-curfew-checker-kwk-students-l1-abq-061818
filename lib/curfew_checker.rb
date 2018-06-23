e8ae4953da13aa7ecf845e9d8117d45968b0d72c
Myakkatt <myao_03@icloud.com>
2018-06-23 01:04:00 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,12 +1,23 @@
-def simple_curfew_checker(time)
-  # code goes here
+def simple_curfew_checker(9:30)
+  if simple_curfew_checker 
+    puts "get ready for bed"
+  else simple_curfew_checker
+    puts "not yet time for bed"
+  end
 end
 
-def curfew_checker(time)
-  # code goes here
+def curfew_checker(10:00)
+  if 10 < 11
+    puts "Really it's time to get ready for bed"
+  else 
+    puts "Get ready for bed"
+  end
 end
 
-def complex_curfew_checker(time)
+def complex_curfew_checker(10:30)
+  if 10:30 < 11
+    puts "You should be in bed now"
+    
   # code goes here
 end
 
